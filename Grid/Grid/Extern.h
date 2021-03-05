3b4db3243419a2b4b96f547e9fd2fa51705e9741
SchmidtDavidSimon <schmidtdavidsimon@gmail.com>
2021-03-05 12:33:22 +0100

    Finished Commenting

--- Grid/Grid/Extern.h
+++ Grid/Grid/Extern.h
@@ -13,26 +13,100 @@ namespace Extern
 {
 	extern "C"
 	{
+		/// <summary>
+		/// Creates a pointer to a grid class and casts it into an int*
+		/// </summary>
+		/// <param name="width">Width of the grid</param>
+		/// <param name="height">Height of the Grid</param>
+		/// <param name="defaultValue">Default Value the grid is filled with</param>
+		/// <param name="outOfBoundsValue">Value to compare OutOfBounds values to</param>
 		dllFunc int* CreateGrid(int width, int height, int defaultValue = -1, int outOfBoundsValue = INT_MIN);
+		
+		/// <summary>
+		/// Casts the given int* into a Grid* and delets it
+		/// </summary>
 		dllFunc void DeleteGrid(int* grid);
-
+		
+		/// <summary>
+		/// Casts the given int* into a Grid* and returns its Width
+		/// </summary>
 		dllFunc int GetWidth(int* grid);
+
+		/// <summary>
+		/// Casts the given int* into a Grid* and returns its Height
+		/// </summary>
 		dllFunc int GetHeight(int* grid);
+
+		/// <summary>
+		/// Casts the given int* into a Grid* and returns its DefaultValue
+		/// </summary>
 		dllFunc int GetDefaultValue(int* grid);
+
+		/// <summary>
+		/// Casts the given int* into a Grid* and returns its OutOfBoundsValue
+		/// </summary>
 		dllFunc int GetOutOfBoundsValue(int* grid);
 		
+		/// <summary>
+		/// Casts the given int* into a Grid* and returns the saved int on a given coordinate
+		/// </summary>
 		dllFunc int GetCoordinateContent(int* grid, int x, int y);
+
+		/// <summary>
+		/// Casts the given int* into a Grid* and sets the value of the given coordinate to the given value
+		/// </summary>
 		dllFunc void SetCoordinateContent(int* grid, int x, int y, int value);
 		
+		/// <summary>
+		/// Casts the given int* into a Grid* and returns an int* with x and y of a random coordinate of the given type
+		/// </summary>
 		dllFunc int* GetRandomCoordinateOfValue(int* grid, int value);
 
+		/// <summary>
+		/// Casts the given int* into a Grid* and returns an int* with adjaciant values. The int* is structured as follows:
+		/// {LEFT,TOP,RIGHT,BOTTOM}
+		/// </summary>
 		dllFunc int* GetAdjacentValues(int* grid, int x, int y);
+
+		/// <summary>
+		/// Casts the given int* into a Grid* and returns an int* with adjacent valid coordinates. The int* is structured as follows:
+		/// [0] = Num of valid coordinates
+		/// [1] = X Coordinate of 1. valid Coordinate
+		/// [2] = Y Coordinate of 1. valid Coordinate
+		/// Repeat [1]&[2] for each valid Coordinate
+		/// </summary>
 		dllFunc int* GetAdjacentValidCoordinates(int* grid, int x, int y);
+
+		/// <summary>
+		/// Casts the given int* into a Grid* and returns an int* with adjacent valid coordinates of one of the given values. The int* is structured as follows:
+		/// [0] = Num of valid coordinates
+		/// [1] = X Coordinate of 1. valid Coordinate
+		/// [2] = Y Coordinate of 1. valid Coordinate
+		/// Repeat [1]&[2] for each valid Coordinate
+		/// </summary>
 		dllFunc int* GetAdjacentValidValuesOfTypes(int* grid, int x, int y, int* values);
 		
+		/// <summary>
+		/// Casts the given int* into a Grid* and returns an int* with coordinates on a path from start to end. The int* is structured as follows:
+		/// [0] = Num of coordinates on path
+		/// [1] = X coordinate of 1. Coordinate on the path
+		/// [2] = Y coordinate of 1. Coordinate on the path
+		/// Repeat [1]&[2] for each Coordinate on the path
+		/// </summary>
 		dllFunc int* AStarSearch(int* grid, int startX, int startY, int endX, int endY, bool useCost);
+		
+		/// <summary>
+		/// Casts the given int* into a Grid* and returns an int* with coordinates on a path from start to end by only using values specified in the usableValues on the value Grid. The int* is structured as follows:
+		/// [0] = Num of coordinates on path
+		/// [1] = X coordinate of 1. Coordinate on the path
+		/// [2] = Y coordinate of 1. Coordinate on the path
+		/// Repeat [1]&[2] for each Coordinate on the path
+		/// </summary>
 		dllFunc int* AStarSearchWithTypeInfo(int* grid, int startX, int startY, int endX, int endY, bool useCost, int* usebleValues, int* valueGrid);
 	
+		/// <summary>
+		/// Delete the int* with delete[]
+		/// </summary>
 		dllFunc void DeleteArray(int* arr);
 	}
 }
--- SampleProject/Assets/Scripts/Utilities/GridExtension.cs
+++ SampleProject/Assets/Scripts/Utilities/GridExtension.cs
@@ -190,6 +190,18 @@ namespace Utilities
             return retVal;
         }
 
+        /// <summary>
+        /// Returns a list of all neighbors
+        /// 1. Create the target pointer for the neighbors
+        /// 2. If types is null save all neighbors on the Grid into the target pointer
+        /// 3. If not
+        ///     1. Convert the allowed types into an array
+        ///     2. Convert the array into an IntPtr
+        ///     3. Save all neighbors with one of the given types into the target pointer
+        /// 4. Convert the target pointer into a list of Vector3Ints
+        /// 5.Delete the created pointer
+        /// </summary>
+        /// <param name="types">Types of cells to look for in neighbors. Use null for all types</param>
         public static List<Vector3Int> GetNeighborsOfTypes(Vector3Int cell, [CanBeNull] List<CellContentType> types)
         {
             IntPtr neighbors;
@@ -212,6 +224,19 @@ namespace Utilities
             return retVal;
         }
 
+        /// <summary>
+        /// Returns a list of all cells on a path between the given start and end cell
+        /// 1. Create the target pointer for the path
+        /// 2. If allowedTypes is null save the path of the appropriate grid determined by useCost in the target poniter
+        /// 3. If not
+        ///     1. Convert allowedTypes into an array
+        ///     2. Convert the array into an IntPtr
+        ///     3. Save the path of the appropriate grid determined by useCost un the target poniter
+        /// 4. Convert the target pointer into a list of Vector3Ints
+        /// 5. Delete the target pointer
+        /// </summary>
+        /// <param name="allowedTypes">Types of cells to be used for pathfinding. Use null for all types</param>
+        /// <param name="useCost">Search for path using the cost grid, otherwise use type grid</param>
         public static List<Vector3Int> GetPathOfTypeBetween(Vector3Int start, Vector3Int end, [CanBeNull] List<CellContentType> allowedTypes, bool useCost = false)
         {
             IntPtr path;
@@ -251,6 +276,13 @@ namespace Utilities
 
         #region private methods
 
+        /// <summary>
+        /// For all coordinates of the origin coordinate and the given width and height check if the coordinate is in bounds of the grid.
+        /// If not set the given grid on the calculated coordinate to the given value
+        /// </summary>
+        /// <param name="x">X Coordinate of the origin cell</param>
+        /// <param name="y">Y coordinate of the origin cell</param>
+        /// <returns>Returns true if all cells over widthxheight are in bounds of the array</returns>
         private static bool SetCell(IntPtr grid, int x, int y, int width, int height, int value)
         {
             for (var i = x; i < x + width; i++)
@@ -265,10 +297,23 @@ namespace Utilities
             return true;
         }
 
+        /// <summary>
+        /// Converts a given IntPtr of the correct format written in the dll into a list of Vector3Ints
+        /// 1. Read the first int to determine the size of the pointer in cells
+        /// 2. Create an array for the data to go into, the arrays length is composed as follows
+        ///     {1 for the size + 1x size for x coordinates + 1x size for y coordinates}
+        /// 3. Copy the data from the IntPtr into the array for better usage
+        /// 4. Create a new array with a lenght of 2* size for just the coordinate data
+        /// 5. Fill the new array with the coordinate data
+        /// 6. Create the return value list
+        /// 7. Go throw the new array as follows:
+        ///     1. If an index is even save it in a temp variable
+        ///     2. if an index is odd add a new Vector3Int to the return value with the saved even value as it's x an the current odd value as it's z coordinate
+        /// </summary>
         private static List<Vector3Int> IntPtrToVector3Ints(IntPtr ptr)
         {
             var size = Marshal.ReadInt32(ptr);
-            var temp = new int[size * 2 + 1];
+            var temp = new int[1 + size * 2];
             Marshal.Copy(ptr, temp, 0, size * 2 + 1);
             var neighborsArr = new int[size * 2];
             for (var i = 0; i < size * 2; i++)
@@ -294,6 +339,9 @@ namespace Utilities
             return retVal;
         }
 
+        /// <summary>
+        /// Converts a given array into an IntPtr by using a GCHandle
+        /// </summary>
         private static IntPtr ArrayToIntPtr(int[] arr)
         {
             IntPtr retVal;
@@ -313,9 +361,16 @@ namespace Utilities
             return retVal;
         }
 
+        /// <summary>
+        /// Converts a list of contentTypes into an int array in format for usage by the dll
+        /// 1. Create a new array whose size is composed as follows
+        ///     {1 for the size of the following values + the lists count for the}
+        /// 2. Save the lists count at the index 0 of the array
+        /// 3. Save the types casted into ints into the open indices of the array 
+        /// </summary>
         private static int[] TypeListToIntArray(List<CellContentType> types)
         {
-            var retVal = new int[types.Count + 1];
+            var retVal = new int[1 + types.Count];
             retVal[0] = types.Count;
             for (var i = 1; i < types.Count + 1; i++)
             {
